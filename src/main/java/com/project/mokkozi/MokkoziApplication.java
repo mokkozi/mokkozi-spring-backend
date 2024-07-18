24186e89036986fc3a9c2a96024cbe10a38c7739
zzex3on <zzeonii0612@gmail.com>
2024-07-18 10:44:36 +0900

    🎉 mokkozi 프로젝트 시작

---
+++ src/main/java/com/project/mokkozi/MokkoziApplication.java
@@ -0,0 +1,13 @@
+package com.project.mokkozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokkoziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokkoziApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/mokkozi/repository/MemberRepository.java
@@ -0,0 +1,48 @@
+package com.project.mokkozi.repository;
+
+import com.project.mokkozi.entity.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+// JpaRepository<User, Long> 인터페이스 : 스프링 데이터 JPA에서 제공하는 CRUD 메서드를 상속받아 사용할 수 있는 인터페이스
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByName(String memberName);
+
+    /*
+    private final EntityManager em;
+
+    public void createMember(Member member) {
+        em.persist(member);
+    }
+
+    public void remove(Long id) {
+        em.remove(findById(id));
+    }
+
+    public List<Member> findAllMember() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public Member findByName(String findName) {
+        return em.createQuery("SELECT m FROM Member m WHERE m.name = :findName", Member.class)
+                .setParameter("findName", findName)
+                .getSingleResult();
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+    }*/
+}
