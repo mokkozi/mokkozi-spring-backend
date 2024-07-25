80727067dbdfdf08c3715ba34507c49730adbad5
hyodaeun <hyodaeun@naver.com>
2024-07-25 21:55:55 +0900

    회원가입 시, 중복체크 api 수정중

--- src/main/java/com/project/mokkozi/repository/MemberRepository.java
+++ src/main/java/com/project/mokkozi/repository/MemberRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByName(String memberName);
-
+    Optional<Member> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
 
     /*
